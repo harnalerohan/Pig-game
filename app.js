e71c229ff8fc60854ac4ce5bfdfb8220289548e0
harnalerohan <48817159+harnalerohan@users.noreply.github.com>
2020-06-20 20:36:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.querySelector(".btn-hold").addEventListener("click" ,function() {
     document.querySelector ("#score-" + activePlayer).textContent = scores [activePlayer];
 
     // thech if player has won the game
-    if (scores [activePlayer] >= 20){
+    if (scores [activePlayer] >= 100){
         document.querySelector("#name-" + activePlayer).textContent = "Winner";
         document.querySelector (".dice").style.display = "none";
         document.querySelector (".player-" + activePlayer + "-panel").classList.add("winner");
